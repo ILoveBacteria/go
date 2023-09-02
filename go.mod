bdef7db705a8facab3707d9c218fa707c9ef1bbf
Moein Arabi <moein.mo81@gmail.com>
2023-09-02 11:57:03 +0330

    Update

--- go.mod
+++ go.mod
@@ -2,10 +2,6 @@ module test
 
 go 1.20
 
-require (
-	github.com/jinzhu/inflection v1.0.0 // indirect
-	github.com/jinzhu/now v1.1.5 // indirect
-	github.com/mattn/go-sqlite3 v1.14.17 // indirect
-	gorm.io/driver/sqlite v1.5.2 // indirect
-	gorm.io/gorm v1.25.3 // indirect
-)
+require github.com/gorilla/mux v1.8.0
+
+require golang.org/x/time v0.3.0
--- main.go
+++ main.go
@@ -1,24 +1,11 @@
 package main
 
 import (
-	"gorm.io/driver/sqlite"
-	"gorm.io/gorm"
+	"fmt"
 )
 
-type User struct {
-	gorm.Model
-	name string
-	age  int
-}
-
 func main() {
-	db, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
-	if err != nil {
-		panic("failed to connect database")
-	}
-	err = db.AutoMigrate(&User{})
-	if err != nil {
-		panic("Can't migrate User scheme")
-	}
-	db.Create(&User{name: "Moein", age: 20})
+	var a, b int
+	fmt.Scan(&a, &b)
+	fmt.Print(a * b)
 }
